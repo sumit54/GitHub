37d5d54da3375fbdf4ac65ed84af157d45e4b03d
sumit54 <sumit54@noreply.github.com>
2018-01-09 19:25:33 +0530

    Initial commit

---
+++ Compiler/Compiler.java
@@ -0,0 +1,8 @@
+//Compiler And Interpreter i.e JVM
+class Compiler
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Complier create bytcode and JVM reads file and runs the bytcode");
+	}
+} 
