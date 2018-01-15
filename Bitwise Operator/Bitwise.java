792c503b427bac9703dd5a7afcdb8e5075241b36
sumit54 <sumit54@noreply.github.com>
2018-01-16 00:53:48 +0530

    Bitwise

---
+++ Bitwise Operator/Bitwise.java
@@ -0,0 +1,12 @@
+class BitWise
+{
+	public static void main(String[] args)
+	{
+		int a=4;
+		int b=5;
+		System.out.println(~a);
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a^b);
+	}
+}
---
+++ Bitwise Operator/ShiftOperator.java
@@ -0,0 +1,11 @@
+class ShiftOperator
+{
+	public static void main(String... args)
+	{
+		int a=8;
+		System.out.println(a<<2);
+		System.out.println(a>>2);
+		System.out.println(a>>>2);
+
+	}
+}
